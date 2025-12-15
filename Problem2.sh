a55c6d624d0b64e44a75e6a0fda18de92d01904a
fahim45694 <fahimkhan220099@gmail.com>
2025-12-16 05:04:24 +0600

    Update Problem2.sh

---
+++ Problem2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Enter starting year:"
+read start
+
+echo "Enter ending year:"
+read end
+
+count=0
+
+echo "Leap years between $start and $end are:"
+
+for ((year=start; year<=end; year++))
+do
+    if (( (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0) )); then
+        echo "$year"
+        count=$((count + 1))
+    fi
+done
+
+echo "----------------------------"
+echo "Total number of leap years: $count"
